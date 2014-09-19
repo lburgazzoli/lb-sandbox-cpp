b15ec024b031f1e70988fe99cb76372e0bf0dc3f
lburgazzoli <lburgazzoli@gmail.com>
2014-09-19 10:48:09 +0200

    initial code

---
+++ sbox-common/include/lb_common.hpp
@@ -0,0 +1,18 @@
+#ifndef __LB_COMMON_HPP__
+#define __LB_COMMON_HPP__
+
+namespace lb {
+namespace common {
+
+class Common
+{
+public:
+    Common();
+    virtual ~Common();
+};
+
+} // namespace lb
+} // namespace common
+
+#endif
+
---
+++ sbox-common/src/lb_common.cpp
@@ -0,0 +1,17 @@
+
+#include "lb_common.hpp"
+
+namespace lb {
+namespace common {
+
+Common::Common()
+{
+}
+
+Common::~Common()
+{
+}
+
+} // namespace lb
+} // namespace common
+
